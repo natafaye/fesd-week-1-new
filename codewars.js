8015d7b5b7c6c07c5b8a0b1320cc4f5d9c3729f9
Natalie Childs <nata.faye@gmail.com>
2023-09-13 18:31:09 -0700

    array method codewars

--- codewars.js
+++ codewars.js
@@ -1,62 +1,95 @@
-// Ones and Zeros
-// https://www.codewars.com/kata/578553c3a1b8d5c40300037c/train/javascript
 
-const binaryArrayToNumber = arr => {
-    let total = 0
-    // loop through the array and adding each number times the place it's in to the total
-    for (let i = 0; i < arr.length; i++) {
-        total += (arr[arr.length - i - 1] * Math.pow(2, i))
-    }
-    return total
-};
+// https://www.codewars.com/kata/582dace555a1f4d859000058/train/javascript
 
-// const binaryArrayToNumber = arr => {
-//   // loop through the arry and times each one by the place it's in
-//   for(let i = 0; i < arr.length; i++) {
-//     arr[i] = arr[i] * Math.pow(2, arr.length - i - 1)
-//   }
-//   // Sum them
-//   const total = arr.reduce((total, num) => total + num)
-//   return total
-// };
-
-
-// 1011
-
-// total + 1000 + 0 + 10 + 1
-
-// total += 8 + 0 + 2 + 1
-
-
-
-// Simple Fun: Reverse Letter
-// https://www.codewars.com/kata/58b8c94b7df3f116eb00005b/train/javascript
-
-const letters = "abcdefghijklmnopqrstuvwxyz"
-
-// function reverseLetter(str) {
-//   //coding and coding..
-//   const strArray = str.split("")
-//   const result = []
-  
-//   while(strArray.length) {
-//     const character = strArray.pop()
-//     if(letters.includes(character)) {
-//       result.push(character)
+// function findAdmin(list, lang) {
+//   const matches = []
+//   for(let i = 0; i < list.length; i++) {
+//     if(list[i].githubAdmin === "yes" && list[i].language === lang) {
+//       matches.push( list[i] )
 //     }
 //   }
-  
-//   return result.join("")
+//   return matches
 // }
 
-function reverseLetter(str) {
-  return str
-    .split("") // turn string into an array of characters
-    .reverse() // reverse
-    .filter(character => letters.includes(character)) // only keep letters
-    .join("") // turn array back into string
+function findAdmin(list, lang) {
+    // find takes a callback function that returns true if it's a match and false if it's not a match
+    return list.filter(developer => developer.githubAdmin === "yes" && developer.language === lang)
 }
 
-// function reverseLetter(str) {
-//   return str.split("").reverse().join("").replace(/[^a-z]/ig, "") // /[^a-z]/ig = reg ex for not a-z case insensitively, search the whole string
+
+
+
+
+
+
+
+// function findAdmin(list, lang) {
+//     // find takes a callback function that returns true if it's a match and false if it's not a match
+//     return list.find( developer => developer.githubAdmin === "yes" && developer.language === lang )
 // }
+
+// // somewhere inside find's code
+// callback({ firstName: 'Harry', lastName: 'K.', country: 'Brazil', continent: 'Americas', age: 22, language: 'JavaScript', githubAdmin: 'yes' })
+// callback({ firstName: 'Kseniya', lastName: 'T.', country: 'Belarus', continent: 'Europe', age: 49, language: 'Ruby', githubAdmin: 'no' })
+// callback({ firstName: 'Jing', lastName: 'X.', country: 'China', continent: 'Asia', age: 34, language: 'JavaScript', githubAdmin: 'yes' })
+// callback({ firstName: 'Piotr', lastName: 'B.', country: 'Poland', continent: 'Europe', age: 128, language: 'JavaScript', githubAdmin: 'no' })
+
+
+// describe("somdfdslfds", function() {
+
+// })
+
+
+// let a = first thing passed in, let b = second thing passed
+// const addNumbers = (a, b) => a + b
+
+// let something = 3
+
+
+
+// console.log("3 + 5",   addNumbers(3, 5)   )
+
+// addNumbers(4, 7)
+
+
+
+
+
+
+// var list1 = [
+//     { firstName: 'Harry', lastName: 'K.', country: 'Brazil', continent: 'Americas', age: 22, language: 'JavaScript', githubAdmin: 'yes' },
+//     { firstName: 'Kseniya', lastName: 'T.', country: 'Belarus', continent: 'Europe', age: 49, language: 'Ruby', githubAdmin: 'no' },
+//     { firstName: 'Jing', lastName: 'X.', country: 'China', continent: 'Asia', age: 34, language: 'JavaScript', githubAdmin: 'yes' },
+//     { firstName: 'Piotr', lastName: 'B.', country: 'Poland', continent: 'Europe', age: 128, language: 'JavaScript', githubAdmin: 'no' }
+// ];
+
+
+// console.log("Find JS admins in list 1", findAdmin(list1, "JavaScript"))
+
+
+// var list2 = [
+//     { firstName: 'Harry', lastName: 'K.', country: 'Brazil', continent: 'Americas', age: 22, language: 'Ruby', githubAdmin: 'yes' },
+//     { firstName: 'Kseniya', lastName: 'T.', country: 'Belarus', continent: 'Europe', age: 49, language: 'Ruby', githubAdmin: 'no' },
+//     { firstName: 'Jing', lastName: 'X.', country: 'China', continent: 'Asia', age: 34, language: 'Python', githubAdmin: 'yes' },
+//     { firstName: 'Piotr', lastName: 'B.', country: 'Poland', continent: 'Europe', age: 128, language: 'JavaScript', githubAdmin: 'no' }
+// ];
+
+// console.log("Find python admins in list 2", findAdmin(list2, "Python"))
+
+
+
+
+
+
+
+
+// https://www.codewars.com/kata/58287977ef8d4451f90001a0/train/javascript
+
+function isSameLanguage(list) {
+    const firstDeveloper = list[0]
+
+    // check if all the items in the array match the language of the first developer
+    const matches = list.filter(developer => developer.language === firstDeveloper.language)
+
+    return matches.length === list.length
+}
